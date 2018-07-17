2503c8d8bac66ca7b3eeecd37db0586e9b3e29a7
Scotty Labs <alexander-beer-weiss@users.noreply.github.com>
2018-07-17 08:47:40 -0700

    pycache in gitignore, change some paths in train.sh

--- retinanet/tools/train.sh
+++ retinanet/tools/train.sh
@@ -2,18 +2,18 @@
 set -e
 CUDA_VISIBLE_DEVICES="0"
 WEIGHTS='imagenet'
-TRAIN_PATH='/data/train_data'
-VALIDATION_PATH='/data/train_data'
+TRAIN_PATH='data-link/train_data'
+VALIDATION_PATH='data-link/train_data'
 
 echo 'Parameters:'
 echo 'TRAIN_PATH:' $TRAIN_PATH
 echo 'VALIDATION_PATH:' $VALIDATION_PATH
 echo 'WEIGHTS:' $WEIGHTS
 echo '------------------------------------'
-PYTHONPATH=../../:../../apollo_python_common/protobuf/:$PYTHONPATH
-export PYTHONPATH
+#PYTHONPATH=../../:../../apollo_python_common/protobuf/:$PYTHONPATH
+#export PYTHONPATH
 
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python -u ../train.py \
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python -u retinanet/train.py \
     --imagenet-weights \
     --steps 35000 --multi-gpu 1 --batch-size 1 --evaluate_score_threshold 0.5 \
     traffic_signs $TRAIN_PATH $VALIDATION_PATH
